6cd7bc8952b2bf8a1863a6561883bb931f9ccc18
anh.nguyen92 <nguyentuananh30392@gmail.com>
2022-10-05 13:42:58 +0700

    First commit

---
+++ src/main/java/vn/com/fbsharearticle/FbShareArticleApplication.java
@@ -0,0 +1,13 @@
+package vn.com.fbsharearticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbShareArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbShareArticleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/fbsharearticle/FbShareArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.fbsharearticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbShareArticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
